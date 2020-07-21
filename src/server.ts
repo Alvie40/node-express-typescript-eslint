2f0f9ceee8f3e00ed4646d8bb00edbff6c903838
Alvaro Lopes <alvaro40@gmail.com>
2020-07-21 16:53:23 -0400

    :spark: first commit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('🌎 Server started at port 3333!');
+});
